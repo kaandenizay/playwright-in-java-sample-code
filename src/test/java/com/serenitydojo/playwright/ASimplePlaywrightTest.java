d788614d105b13e98e0a27edd83456a5ce41b140
Kaan Denizay <kaan.denizay.ext@ozan.com>
2025-05-22 17:43:47 +0300

    First test to verify page title

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -8,4 +8,16 @@ import org.junit.jupiter.api.Test;
 
 public class ASimplePlaywrightTest {
 
+    @Test
+    void shouldShowThePageTitle() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com/");
+        String title = page.title();
+        Assertions.assertTrue( title.contains("Practice Software Testing"));
+
+        browser.close();
+    }
 }
